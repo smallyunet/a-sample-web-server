439b78f32e42dad06f9169d9c9fc0dc79651a1a0
smallyunet <499503799@qq.com>
2020-07-14 10:43:13 +0800

    v0.1 linux

---
+++ server.rkt
@@ -0,0 +1,18 @@
+#lang racket
+
+
+(require web-server/servlet
+         web-server/servlet-env)
+
+
+(define (start req)
+  (response/xexpr
+   `(html (head (title "Hello world!"))
+          (body (p "Hey out there!")))))
+
+
+(serve/servlet start
+               #:extra-files-paths
+               (list
+                (build-path "./")))
+
