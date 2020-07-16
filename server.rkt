2b106dfb99e9226d7aad8a8e226e370b20149ea4
smallyunet <499503799@qq.com>
2020-07-16 09:58:12 +0800

    v0.0.2

--- server.rkt
+++ server.rkt
@@ -1,18 +1,34 @@
 #lang racket
 
-
 (require web-server/servlet
          web-server/servlet-env)
 
+(require racket/cmdline)
+
+
+(define port (make-parameter #false))
 
 (define (start req)
   (response/xexpr
    `(html (head (title "Hello world!"))
           (body (p "Hey out there!")))))
 
+(define parser
+  (command-line
+   #:usage-help "hello"
+   #:once-each
+   [("-p" "--port") PORT "Set port" (port PORT)]
+   #:args () (void)))
+
+(define (port-handler p)
+  (cond
+    [(boolean? p) "8000"]
+    [(string? p) (string-append p)]))
+
 
 (serve/servlet start
+               #:port (string->number (port-handler (port)))
                #:extra-files-paths
-               (list
-                (build-path "./")))
+               (list (build-path "./"))
+               #:command-line? #f)
 
